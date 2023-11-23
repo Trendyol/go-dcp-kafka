be1c227ec2a2ae4e682b483cc8019f30ced0305f
mhmtszr <m.szr98@gmail.com>
2023-11-23 13:49:44 +0300

    feat: add cas to connector event

--- couchbase/event.go
+++ couchbase/event.go
@@ -7,37 +7,41 @@ type Event struct {
 	EventTime      time.Time
 	Key            []byte
 	Value          []byte
+	Cas            uint64
 	IsDeleted      bool
 	IsExpired      bool
 	IsMutated      bool
 }
 
-func NewDeleteEvent(key []byte, value []byte, collectionName string, eventTime time.Time) Event {
+func NewDeleteEvent(key []byte, value []byte, collectionName string, eventTime time.Time, cas uint64) Event {
 	return Event{
 		Key:            key,
 		Value:          value,
 		IsDeleted:      true,
 		CollectionName: collectionName,
 		EventTime:      eventTime,
+		Cas:            cas,
 	}
 }
 
-func NewExpireEvent(key []byte, value []byte, collectionName string, eventTime time.Time) Event {
+func NewExpireEvent(key []byte, value []byte, collectionName string, eventTime time.Time, cas uint64) Event {
 	return Event{
 		Key:            key,
 		Value:          value,
 		IsExpired:      true,
 		CollectionName: collectionName,
 		EventTime:      eventTime,
+		Cas:            cas,
 	}
 }
 
-func NewMutateEvent(key []byte, value []byte, collectionName string, eventTime time.Time) Event {
+func NewMutateEvent(key []byte, value []byte, collectionName string, eventTime time.Time, cas uint64) Event {
 	return Event{
 		Key:            key,
 		Value:          value,
 		IsMutated:      true,
 		CollectionName: collectionName,
 		EventTime:      eventTime,
+		Cas:            cas,
 	}
 }
