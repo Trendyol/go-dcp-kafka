c8a052dbdce8c50ab36c4d1bc88e2c7968d38476
Erdinç Özdemir <erdincozdemir@sabanciuniv.edu>
2024-01-12 13:19:53 +0300

    feat: add VbID to couchbase event (#82)

--- connector.go
+++ connector.go
@@ -56,11 +56,11 @@ func (c *connector) produce(ctx *models.ListenerContext) {
 	var e couchbase.Event
 	switch event := ctx.Event.(type) {
 	case models.DcpMutation:
-		e = couchbase.NewMutateEvent(event.Key, event.Value, event.CollectionName, event.EventTime, event.Cas)
+		e = couchbase.NewMutateEvent(event.Key, event.Value, event.CollectionName, event.EventTime, event.Cas, event.VbID)
 	case models.DcpExpiration:
-		e = couchbase.NewExpireEvent(event.Key, nil, event.CollectionName, event.EventTime, event.Cas)
+		e = couchbase.NewExpireEvent(event.Key, nil, event.CollectionName, event.EventTime, event.Cas, event.VbID)
 	case models.DcpDeletion:
-		e = couchbase.NewDeleteEvent(event.Key, nil, event.CollectionName, event.EventTime, event.Cas)
+		e = couchbase.NewDeleteEvent(event.Key, nil, event.CollectionName, event.EventTime, event.Cas, event.VbID)
 	default:
 		return
 	}
