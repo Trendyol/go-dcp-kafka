44efdf05cb5cc834c0c98f3adca6b0b8ecdb1b80
msimalunsal <msimalunsal@gmail.com>
2025-07-29 17:47:23 +0300

    fix graceful shutdown finite mode

--- dcp_event_handler.go
+++ dcp_event_handler.go
@@ -4,6 +4,7 @@ import "github.com/Trendyol/go-dcp-kafka/kafka/producer"
 
 type DcpEventHandler struct {
 	producerBatch *producer.Batch
+	isFinite      bool
 }
 
 func (h *DcpEventHandler) BeforeRebalanceStart() {
@@ -26,6 +27,9 @@ func (h *DcpEventHandler) AfterStreamStart() {
 }
 
 func (h *DcpEventHandler) BeforeStreamStop() {
+	if h.isFinite {
+		return
+	}
 	h.producerBatch.PrepareStartRebalancing()
 }
 
